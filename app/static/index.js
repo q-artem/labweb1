8b42410d041d791bfb86f94c015033c206fcaa93
roflex <Stodvalista@yandex.ru>
2025-09-29 23:15:51 +0300

    to pc + start adding dop 1

--- app/static/index.js
+++ app/static/index.js
@@ -6,17 +6,16 @@ function getVar(variableName) {
         .trim();
 }
 
-
 let centerX, centerY, R;
 
 function drawShapes() {
-    const canvas = document.getElementById('background');
+    const canvas = document.getElementById("background");
     const container = canvas.parentElement;
 
     canvas.width = container.clientWidth;
     canvas.height = container.clientHeight;
 
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
     ctx.imageSmoothingEnabled = true;
     const width = canvas.width;
     const height = canvas.height;
@@ -91,8 +90,8 @@ function drawShapes() {
     ctx.stroke();
 
     ctx.font = "bold 18px Noto Sans";
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'top';
+    ctx.textAlign = "center";
+    ctx.textBaseline = "top";
 
     ctx.beginPath();
     ctx.moveTo(centerX - R, centerY - 4);
@@ -118,8 +117,8 @@ function drawShapes() {
     ctx.stroke();
     ctx.fillText("R/2", centerX + R / 2, centerY + 6);
 
-    ctx.textAlign = 'left';
-    ctx.textBaseline = 'middle';
+    ctx.textAlign = "left";
+    ctx.textBaseline = "middle";
 
     ctx.beginPath();
     ctx.moveTo(centerX - 4, centerY - R);
@@ -147,24 +146,24 @@ function drawShapes() {
 }
 
 // Инициализация при загрузке
-document.addEventListener('DOMContentLoaded', drawShapes);
+document.addEventListener("DOMContentLoaded", drawShapes);
 
 // Перерисовка при изменении размера окна
-window.addEventListener('resize', drawShapes);
+window.addEventListener("resize", drawShapes);
 
 // Создаем MediaQueryList объект
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 // Функция-обработчик
 function handleThemeChange(event) {
     if (event.matches) {
-        console.log('Системная тема изменилась на: ТЕМНУЮ');
+        console.log("Системная тема изменилась на: ТЕМНУЮ");
         // Действия для темной темы
-        document.body.classList.add('dark-theme');
+        document.body.classList.add("dark-theme");
     } else {
-        console.log('Системная тема изменилась на: СВЕТЛУЮ');
+        console.log("Системная тема изменилась на: СВЕТЛУЮ");
         // Действия для светлой темы
-        document.body.classList.remove('dark-theme');
+        document.body.classList.remove("dark-theme");
     }
 
     // Перерисовываем элементы, зависящие от темы
@@ -172,24 +171,23 @@ function handleThemeChange(event) {
 }
 
 // Добавляем слушатель
-darkModeMediaQuery.addEventListener('change', handleThemeChange);
+darkModeMediaQuery.addEventListener("change", handleThemeChange);
 
 // Вызываем сразу при загрузке
 handleThemeChange(darkModeMediaQuery);
 
-
-const resultTable = document.getElementById('result-table');
+const resultTable = document.getElementById("result-table");
 const possibleXValues = [-2.0, -1.5, -1.0, -0.5, 0, 0.5, 1.0, 1.5, 2.0];
 const possibleRValues = [1, 2, 3, 4, 5];
 
 function validateY(y) {
-    if (y === '') {
-        alert('Y не может быть пустым');
+    if (y === "") {
+        alert("Y не может быть пустым");
         return false;
     }
     const yValue = parseFloat(y);
     if (isNaN(yValue) || yValue < -3 || yValue > 3) {
-        alert('Y должен быть числом в диапазоне от -5 до 3');
+        alert("Y должен быть числом в диапазоне от -5 до 3");
         return false;
     }
     return true;
@@ -197,7 +195,10 @@ function validateY(y) {
 
 function validateX(x) {
     if (!possibleXValues.includes(x)) {
-        alert('X должен быть одним из следующих значений: ' + possibleXValues.join(', '));
+        alert(
+            "X должен быть одним из следующих значений: " +
+                possibleXValues.join(", "),
+        );
         return false;
     }
     return true;
@@ -205,18 +206,20 @@ function validateX(x) {
 
 function validateRs(r) {
     if (!Array.isArray(r) || r.length === 0) {
-        alert('R должен быть массивом с хотя бы одним значением');
+        alert("R должен быть массивом с хотя бы одним значением");
         return false;
     }
     for (let value of r) {
         if (!possibleRValues.includes(value)) {
-            alert('Каждое значение R должно быть одним из следующих: ' + possibleRValues.join(', '));
+            alert(
+                "Каждое значение R должно быть одним из следующих: " +
+                    possibleRValues.join(", "),
+            );
             return false;
         }
     }
     return true;
 }
-
 
 function addResultRow(x, y, r, result, currentTime, executionTime) {
     console.log("Jopa1");
@@ -226,79 +229,88 @@ function addResultRow(x, y, r, result, currentTime, executionTime) {
     row.dataset.r = r;
     row.dataset.hit = result;
 
-    const btn = document.createElement('button');
-    btn.textContent = 'Отобразить';
-    btn.addEventListener('click', () => showPointAnimated(row));
-    btn.className = 'show-btn';
+    const btn = document.createElement("button");
+    btn.textContent = "Отобразить";
+    btn.addEventListener("click", () => showPointAnimated(row));
+    btn.className = "show-btn";
     row.insertCell(0).appendChild(btn);
     row.insertCell(1).innerText = x;
     row.insertCell(2).innerText = y;
     row.insertCell(3).innerText = r;
     row.insertCell(4).innerText = currentTime;
-    row.insertCell(5).innerText = executionTime + ' мс';
-    row.insertCell(6).innerText = result ? 'Попадание' : 'Промах';
+    row.insertCell(5).innerText = executionTime + " мс";
+    row.insertCell(6).innerText = result ? "Попадание" : "Промах";
 
     showPoint(row);
     console.log("Jopa");
 }
 
+document
+    .getElementById("data-form")
+    .addEventListener("submit", async function (event) {
+        event.preventDefault();
 
-document.getElementById("data-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
+        let x = parseFloat(document.getElementById("x").value);
+        let y = document.getElementById("y").value.trim().replace(",", ".");
+        let r = Array.from(
+            document.querySelectorAll('input[name="r"]:checked'),
+        ).map((input) => parseFloat(input.value));
 
-    let x = parseFloat(document.getElementById("x").value);
-    let y = document.getElementById("y").value.trim().replace(',', '.');
-    let r = Array.from(document.querySelectorAll('input[name="r"]:checked')).map(input => parseFloat(input.value));
-
-    if (!validateX(x) || !validateY(y) || !validateRs(r)) {
-        return;
-    }
-
-    const params = new URLSearchParams();
-    params.append('X', x);
-    params.append('Y', y);
-    params.append('R', r.join(','));
-
-    const response = await fetch("/check", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: params.toString()
-    });
-
-    const data = await response.json();
-    console.log(data);
-    console.log("Megajopa");
-    if (response.ok) {
-        for (let i = 0; i < r.length; i++) {
-            addResultRow(data.results[i].X, data.results[i].Y, data.results[i].R, data.results[i].hit, data.current_time, data.processing_time_ms);
+        if (!validateX(x) || !validateY(y) || !validateRs(r)) {
+            return;
         }
-    } else {
-        alert("Ошибка: " + data.message);
-    }
-});
 
+        const params = new URLSearchParams();
+        params.append("X", x);
+        params.append("Y", y);
+        params.append("R", r.join(","));
+
+        const response = await fetch("/check", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: params.toString(),
+        });
+
+        const data = await response.json();
+        console.log(data);
+        console.log("Megajopa");
+        if (response.ok) {
+            for (let i = 0; i < r.length; i++) {
+                addResultRow(
+                    data.results[i].X,
+                    data.results[i].Y,
+                    data.results[i].R,
+                    data.results[i].hit,
+                    data.current_time,
+                    data.processing_time_ms,
+                );
+            }
+        } else {
+            alert("Ошибка: " + data.message);
+        }
+    });
 
 function showPointAnimated(tableRow) {
     const x_value = parseFloat(tableRow.dataset.x);
     const y_value = parseFloat(tableRow.dataset.y);
     const r_value = parseFloat(tableRow.dataset.r);
 
-    let x = centerX + (x_value * (R / r_value));
-    let y = centerY - (y_value * (R / r_value));
+    let x = centerX + x_value * (R / r_value);
+    let y = centerY - y_value * (R / r_value);
 
-    const canvas = document.getElementById('animate-ground');
-    const ctx = canvas.getContext('2d');
+    const canvas = document.getElementById("animate-ground");
+    const ctx = canvas.getContext("2d");
 
-    const container = document.querySelector('.canvas-container');
+    const container = document.querySelector(".canvas-container");
     const size = container.clientWidth;
     if (canvas.width !== size) {
         canvas.width = size;
         canvas.height = size;
     }
 
-    const pointRadius = 5;
+    const pointRadius = 7;
     let scale = 0;
     const maxScale = 1.5;
     const duration = 1200;
@@ -310,54 +322,92 @@ function showPointAnimated(tableRow) {
 
     const animate = () => {
         step++;
-        scale = Math.min(maxScale, step / steps * maxScale);
+        scale = Math.min(maxScale, (step / steps) * maxScale);
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         ctx.beginPath();
-        left_const = 3
-        right_const = 6
-        left_border = left_const * step / steps
-        right_border = right_const * step / steps
-        if (step < (steps/2)) {
-            ctx.arc(x, y, pointRadius, left_border % 2 * Math.PI, right_border % 2 * Math.PI);
+        left_const = 3;
+        right_const = 6;
+        left_border = (left_const * step) / steps;
+        right_border = (right_const * step) / steps;
+        if (step < steps / 2) {
+            ctx.arc(
+                x,
+                y,
+                pointRadius,
+                (left_border % 2) * Math.PI,
+                (right_border % 2) * Math.PI,
+            );
         } else {
-            left_border = left_const * 0.5 + right_const * (step - steps/2) / steps
-            right_border = right_const * 0.5 + left_const * (step - steps/2) / steps
-            ctx.arc(x, y, pointRadius, left_border % 2 * Math.PI, right_border % 2 * Math.PI);
+            left_border =
+                left_const * 0.5 + (right_const * (step - steps / 2)) / steps;
+            right_border =
+                right_const * 0.5 + (left_const * (step - steps / 2)) / steps;
+            ctx.arc(
+                x,
+                y,
+                pointRadius,
+                (left_border % 2) * Math.PI,
+                (right_border % 2) * Math.PI,
+            );
         }
-        ctx.strokeStyle = '#4fbda1';
+        ctx.strokeStyle = "#4fbda1";
         ctx.lineWidth = 5;
         ctx.stroke();
 
+        ctx.beginPath();
+        if (step < steps / 2) {
+            ctx.arc(
+                x,
+                y,
+                16,
+                ((left_border + 1.7) % 2) * Math.PI,
+                ((right_border + 1.7) % 2) * Math.PI,
+            );
+        } else {
+            left_border =
+                left_const * 0.5 + (right_const * (step - steps / 2)) / steps;
+            right_border =
+                right_const * 0.5 + (left_const * (step - steps / 2)) / steps;
+            ctx.arc(
+                x,
+                y,
+                16,
+                ((left_border + 1.7) % 2) * Math.PI,
+                ((right_border + 1.7) % 2) * Math.PI,
+            );
+        }
+        ctx.strokeStyle = "#4fbda1";
+        ctx.lineWidth = 7;
+        ctx.stroke();
 
         if (step < steps) {
-            if (step + 1 === steps) ctx.clearRect(0, 0, canvas.width, canvas.height);
+            if (step + 1 === steps)
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
             requestAnimationFrame(animate);
         }
     };
-
     animate();
 }
-
 
 function showPoint(tableRow) {
     const x_value = parseFloat(tableRow.cells[1].innerText);
     const y_value = parseFloat(tableRow.cells[2].innerText);
     const r_value = parseFloat(tableRow.cells[3].innerText);
-    const hit = tableRow.cells[6].innerText === 'Попадание';
+    const hit = tableRow.cells[6].innerText === "Попадание";
 
     // Пересчет координат
-    let x = centerX + (x_value * (R / r_value));
-    let y = centerY - (y_value * (R / r_value));
+    let x = centerX + x_value * (R / r_value);
+    let y = centerY - y_value * (R / r_value);
 
-    console.log('Координаты на canvas:', {x, y});
+    console.log("Координаты на canvas:", { x, y });
 
-    const canvas = document.getElementById('foreground');
-    const ctx = canvas.getContext('2d');
+    const canvas = document.getElementById("foreground");
+    const ctx = canvas.getContext("2d");
 
     // Убедимся, что canvas правильного размера
-    const container = document.querySelector('.canvas-container');
+    const container = document.querySelector(".canvas-container");
     const size = container.clientWidth;
     const newSize = container.clientWidth;
     if (canvas.width !== size) {
@@ -369,27 +419,70 @@ function showPoint(tableRow) {
 
     ctx.beginPath();
     // Если за пределами канваса
-    if (x < pointRadius || y < pointRadius || x > size - pointRadius || y > size - pointRadius) {
-        if (x< pointRadius) x = pointRadius;
+    if (
+        x < pointRadius ||
+        y < pointRadius ||
+        x > size - pointRadius ||
+        y > size - pointRadius
+    ) {
+        if (x < pointRadius) x = pointRadius;
         if (y < pointRadius) y = pointRadius;
         if (x > size - pointRadius) x = size - pointRadius;
         if (y > size - pointRadius) y = size - pointRadius;
-        ctx.moveTo(x, y - pointRadius*1.2); // Верхняя вершина
-        ctx.lineTo(x - pointRadius*1.2, y + pointRadius*1.2); // Левая нижняя
-        ctx.lineTo(x + pointRadius*1.2, y + pointRadius*1.2); // Правая нижняя
+        ctx.moveTo(x, y - pointRadius * 1.2); // Верхняя вершина
+        ctx.lineTo(x - pointRadius * 1.2, y + pointRadius * 1.2); // Левая нижняя
+        ctx.lineTo(x + pointRadius * 1.2, y + pointRadius * 1.2); // Правая нижняя
         ctx.closePath();
         ctx.fillStyle = "#ccbe2c"; // Более яркие цвета
     } else {
         ctx.arc(x, y, pointRadius, 0, Math.PI * 2); // Увеличим радиус для видимости
-        ctx.fillStyle = hit ? '#53CA61' : '#FF3333'; // Более яркие цвета
+        ctx.fillStyle = hit ? "#53CA61" : "#FF3333"; // Более яркие цвета
     }
 
     ctx.fill();
-    ctx.strokeStyle = '#000';
+    ctx.strokeStyle = "#000";
     ctx.lineWidth = 1.5;
     ctx.stroke();
 
-    console.log('Точка нарисована');
+    console.log("Точка нарисована");
 }
 
+// battarey API
+if (navigator.getBattery) {
+    navigator
+        .getBattery()
+        .then((battery) => {
+            const statusElement = document.getElementById("battery-status");
 
+            function updateBatteryStatus() {
+                const level = battery.level * 100; // в процентах
+                statusElement.textContent = `Уровень заряда: ${Math.round(level)}%`;
+
+                // Если заряд <= 35% → включаем монохромный режим
+                if (level <= 35) {
+                    document.body.classList.add("monochrome");
+                    statusElement.textContent +=
+                        " — Включен монохромный режим энергосбережения";
+                } else {
+                    document.body.classList.remove("monochrome");
+                }
+
+                console.log(`Батарея: ${level.toFixed(1)}%`);
+            }
+
+            // Обновляем при изменении
+            battery.addEventListener("chargingchange", updateBatteryStatus);
+            battery.addEventListener("levelchange", updateBatteryStatus);
+
+            // Первое обновление
+            updateBatteryStatus();
+        })
+        .catch((err) => {
+            console.error("Ошибка получения данных о батарее:", err);
+            document.getElementById("battery-status").textContent =
+                "❌ API батареи не доступен. Уровень заряда нельзя определить.";
+        });
+} else {
+    document.getElementById("battery-status").textContent =
+        "❌ Браузер не поддерживает Battery Status API.";
+}
